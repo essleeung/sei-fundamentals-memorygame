511b513079c1ef2a061aaeb6a6e7439a192073df
essleeung <essleeung@gmail.com>
2020-02-27 23:36:53 -0500

    created game logic

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-let cardOne = 'queen';
-let cardTwo = 'queen';
-let cardThree = 'king';
-let cardFour = 'king';
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
 
+//user flipped queen
+let cardOne = cards[3];
+cardsInPlay.push(cardOne);
 console.log("user flipped " + cardOne);
-console.log("user flipped " + cardFour);
+
+//user flipped king
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardTwo)
+
+//logic to see if two cards in play
+if (cardsInPlay.length === 2) {
+    if (cardOne === cardTwo) {
+        alert("you found a match!");
+    } else {
+        alert("please try again");
+    }
+}
